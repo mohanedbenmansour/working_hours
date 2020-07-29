0f2ffe112299c7d3b1b7d96dea78b11fb6441a89
mohaned benmansour <mohaned.benmansour@esprit.tn>
2020-07-29 02:40:40 +0100

    preparing the add working hours interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,25 @@ import { ViewPostsComponent } from './components/view-posts/view-posts.component
 import { Ng5SliderModule } from 'ng5-slider';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { SliderComponent } from './components/slider/slider.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, AddPostComponent, ViewPostsComponent],
+  declarations: [
+    AppComponent,
+    AddPostComponent,
+    ViewPostsComponent,
+    SliderComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     Ng5SliderModule,
     MatSlideToggleModule,
     BrowserAnimationsModule,
+    MatCardModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/add-post/add-post.component.ts
+++ src/app/components/add-post/add-post.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Options, LabelType } from 'ng5-slider';
-
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
@@ -9,23 +8,21 @@ import { Options, LabelType } from 'ng5-slider';
 export class AddPostComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
-  value: number = 7;
-  highValue: number = 9;
+  ngOnInit(): void {
+    console.log(this.value[0]);
+  }
+  monday = true;
+  tuesday = false;
+  wednesday = false;
+  thursday = false;
+  friday = false;
+  saturday = false;
+  sunday = false;
+  value: Array<number> = [7, 7, 7, 7, 7, 7, 7];
+  highValue: Array<number> = [9, 9, 9, 9, 9, 9, 9];
   options: Options = {
     floor: 6,
     ceil: 12,
     step: 0.5,
-    showTicks: true,
-    // translate: (value: number, label: LabelType): string => {
-    //   switch (label) {
-    //     case LabelType.Low:
-    //       return '<b>Min price:</b> $' + value;
-    //     case LabelType.High:
-    //       return '<b>Max price:</b> $' + value;
-    //     default:
-    //       return '$' + value;
-    //   }
-    // },
   };
 }
